d5f84a5383f01c85285f919105c73305d2a1b6e9
Alexander Lourier <aml@rulezz.ru>
2011-12-25 12:08:22 +0300

    Error messages in quest engine parser

--- mg/mmorpg/quest_parser.py
+++ mg/mmorpg/quest_parser.py
@@ -263,13 +263,13 @@ class QuestAction(Parsing.Nonterm):
         char = get_attr(call, "call", attrs, "char")
         validate_attrs(call, "call", attrs, ["quest", "event", "char"])
         if not re_valid_identifier.match(event):
-            raise Parsing.SyntaxError(any_obj.script_parser._("Event identifier must start with latin letter or '_'. Other symbols may be latin letters, digits or '_'"))
+            raise Parsing.SyntaxError(call.script_parser._("Event identifier must start with latin letter or '_'. Other symbols may be latin letters, digits or '_'"))
         args = {}
         if char is not None:
             args["char"] = char
         if quest:
             if not re_valid_identifier.match(quest):
-                raise Parsing.SyntaxError(any_obj.script_parser._("Quest identifier must start with latin letter or '_'. Other symbols may be latin letters, digits or '_'"))
+                raise Parsing.SyntaxError(call.script_parser._("Quest identifier must start with latin letter or '_'. Other symbols may be latin letters, digits or '_'"))
             self.val = ["call2", quest, event, args]
         else:
             self.val = ["call2", None, event, args]
