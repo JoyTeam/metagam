2be1db40af45467d0e0d19ab82866ff6c4e0cbb1
Alexander Lourier <aml@rulezz.ru>
2012-02-07 14:35:50 +0300

    Memtable tuning

--- mg/core/cass.py
+++ mg/core/cass.py
@@ -143,9 +143,10 @@ class Cassandra(object):
                             cfdef.key_cache_save_period_in_seconds = 3600
                             cfdef.memtable_throughput_in_mb = 1
                             cfdef.memtable_operations_in_millions = 10000 / 1e6
+                            cfdef.memtable_flush_after_mins = 10
                             cfdef.gc_grace_seconds = 86400 * 10
-                            cfdef.min_compaction_threshold = 2
-                            cfdef.max_compaction_threshold = 4
+                            cfdef.min_compaction_threshold = 1
+                            cfdef.max_compaction_threshold = 2
                             sys_conn = self.pool.sys_connection()
                             sys_conn.cass.set_keyspace("system")
                             logger.debug("Created column family %s.%s: %s", self.keyspace, cfdef.name, sys_conn.cass.system_add_column_family(cfdef))
