0abaafed37467c79fcb6a285cb76bacda118374c
Alexander Lourier <aml@rulezz.ru>
2012-02-10 18:19:10 +0300

    Disabled debug in chat

--- mg/constructor/chat.py
+++ mg/constructor/chat.py
@@ -879,7 +879,7 @@ class Chat(ConstructorModule):
                     channel_id = "sys"
                 lst = self.objlist(DBChatChannelCharacterList, query_index="channel", query_equal=channel_id)
                 character_uuids = [re_after_dash.sub('', uuid) for uuid in lst.uuids()]
-                self.debug("Character %s init. sending roster for channel %s: %s", character.name, channel_id, character_uuids)
+                #self.debug("Character %s init. sending roster for channel %s: %s", character.name, channel_id, character_uuids)
                 for char_uuid in character_uuids:
                     char = self.character(char_uuid)
                     self.call("stream.packet", syschannel, "chat", "roster_add", character=self.roster_info(char), channel=channel["id"])
@@ -919,7 +919,7 @@ class Chat(ConstructorModule):
 
     def channel_join(self, character, channel, send_myself=True):
         channel_id = channel["id"]
-        self.debug("Character %s is attempting to join channel %s (chatmode %s)", character.name, channel_id, self.chatmode)
+        #self.debug("Character %s is attempting to join channel %s (chatmode %s)", character.name, channel_id, self.chatmode)
         if self.chatmode == 0 and channel_id != "sys":
             return
         if channel_id == "loc":
@@ -936,13 +936,13 @@ class Chat(ConstructorModule):
                 obj.set("roster_info", self.roster_info(character))
             obj.store()
             if channel.get("roster"):
-                self.debug("Character %s is joining channel %s with roster", character.name, channel_id)
+                #self.debug("Character %s is joining channel %s with roster", character.name, channel_id)
                 # list of characters subscribed to this channel
                 lst = self.objlist(DBChatChannelCharacterList, query_index="channel", query_equal=channel_id)
                 character_uuids = [re_after_dash.sub('', uuid) for uuid in lst.uuids()]
                 if not send_myself:
                     character_uuids = [uuid for uuid in character_uuids if uuid != character.uuid]
-                self.debug("Subscribed characters: %s", character_uuids)
+                #self.debug("Subscribed characters: %s", character_uuids)
                 if len(character_uuids):
                     # load sessions of these characters
                     lst = self.objlist(SessionList, query_index="authorized_user", query_equal=["1-%s" % uuid for uuid in character_uuids])
@@ -966,7 +966,7 @@ class Chat(ConstructorModule):
                                     self.call("stream.packet", ch, "chat", "roster_add", character=self.roster_info(char), channel=roster_channel_id)
                         else:
                             # dropping obsolete database record
-                            self.debug("Unjoining offline character %s from channel %s", char_uuid, channel_id)
+                            #self.debug("Unjoining offline character %s from channel %s", char_uuid, channel_id)
                             obj = self.obj(DBChatChannelCharacter, "%s-%s" % (char_uuid, channel_id), silent=True)
                             obj.remove()
             else:
@@ -1000,11 +1000,11 @@ class Chat(ConstructorModule):
                     # dropping obsolete database records
                     for char_uuid in character_uuids:
                         if char_uuid not in characters_online:
-                            self.debug("Unjoining offline character %s from channel %s", char_uuid, channel_id)
+                            #self.debug("Unjoining offline character %s from channel %s", char_uuid, channel_id)
                             obj = self.obj(DBChatChannelCharacter, "%s-%s" % (char_uuid, channel_id), silent=True)
                             obj.remove()
             # dropping database record
-            self.debug("Unjoining character %s from channel %s", character.uuid, channel_id)
+            #self.debug("Unjoining character %s from channel %s", character.uuid, channel_id)
             obj = self.obj(DBChatChannelCharacter, "%s-%s" % (character.uuid, channel_id), silent=True)
             obj.remove()
 
