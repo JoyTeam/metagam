c42998e29d6bd2f1407643231c54b6eaf548cd57
Alexander Lourier <aml@rulezz.ru>
2013-02-01 09:09:44 +0400

    Enforce SSL on mmoconstructor main site

--- mg/constructor/admin.py
+++ mg/constructor/admin.py
@@ -70,6 +70,7 @@ class Constructor(Module):
             "mg.constructor.marketing.GoogleAnalytics", "mg.constructor.marketing.GoogleAnalyticsAdmin",
             "mg.constructor.socialnets.SocialNets", "mg.constructor.socialnets.SocialNetsAdmin",
             "mg.core.monitoring.ClusterMonitor",
+            "mg.core.ssl.RedirectSSLModule",
         ])
         self.rhook("web.setup_design", self.web_setup_design)
         self.rhook("ext-index.index", self.index, priv="public")
