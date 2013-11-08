467a00553878e3b5b53c1f8cb95c2a4b640aea1f
Alexander Lourier <aml@rulezz.ru>
2013-11-08 13:39:03 +0400

    Peaceful activities configuration menu entry removed

--- mg/mmorpg/peaceful.py
+++ mg/mmorpg/peaceful.py
@@ -22,17 +22,7 @@ class Peaceful(ConstructorModule):
 
 class PeacefulAdmin(ConstructorModule):
     def register(self):
-        self.rhook("permissions.list", self.permissions_list)
         self.rhook("menu-admin-root.index", self.menu_root_index)
-        self.rhook("menu-admin-peaceful.index", self.menu_peaceful_index)
-
-    def permissions_list(self, perms):
-        perms.append({"id": "peaceful.config", "name": self._("Peaceful activities: configuration")})
 
     def menu_root_index(self, menu):
         menu.append({"id": "peaceful.index", "text": self._("Peaceful activities"), "order": 22})
-
-    def menu_peaceful_index(self, menu):
-        req = self.req()
-        if req.has_access("peaceful.config"):
-            menu.append({"id": "peaceful/config", "text": self._("Peaceful activities configuration"), "order": 0, "leaf": True})
