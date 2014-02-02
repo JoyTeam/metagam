d7e170c36322770c8d98a1210f923c01a90d6845
Alexander Lourier <aml@rulezz.ru>
2014-02-02 23:41:50 +0100

    GNU GPL release

--- mg/socio/restraints.py
+++ mg/socio/restraints.py
@@ -1,3 +1,20 @@
+#!/usr/bin/python2.6
+
+# This file is a part of Metagam project.
+#
+# Metagam is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+# 
+# Metagam is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Metagam.  If not, see <http://www.gnu.org/licenses/>.
+
 from mg import *
 from mg.core.auth import AuthLogList, DBBanIP, DBBanIPList
 import re
